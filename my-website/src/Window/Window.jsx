7caa3af45e45c22f34f6019823833b509ba0f5ee
sarakok <kok.sara777@gmail.com>
2025-07-15 20:54:26 +1000

    Added quicklinks to window and their pictures - to be changed later

--- my-website/src/Window/Window.jsx
+++ my-website/src/Window/Window.jsx
@@ -1,4 +1,5 @@
 import styles from "./Window.module.css";
+import myProjectsImg from "../assets/my-projects.jpeg";
 
 function Window() {
     return (
@@ -8,6 +9,34 @@ function Window() {
                     Welcome home!
                     <div className={styles.closeButton}>x</div>
                 </div>
+                <div className={styles.quickLinksContainer}>
+
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">My Projects</a>
+                    </div>
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">Gallery</a>
+                    </div>
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">Store</a>
+                    </div>
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">About Me</a>
+                    </div>
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">Contact Me</a>
+                    </div>
+                    <div className={styles.quickLinksItem}>
+                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
+                        <a href="#">Your Cart</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
