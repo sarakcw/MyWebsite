af4b95a13076ded1c0fbb6e52a26c30397ccebf1
sarakok <kok.sara777@gmail.com>
2025-07-21 15:25:40 +1000

    Created new quicklinks component to practice using props

--- my-website/src/Window/WindowHome.jsx
+++ my-website/src/Window/WindowHome.jsx
@@ -1,5 +1,11 @@
 import styles from "./Window.module.css";
-import myProjectsImg from "../assets/my-projects.jpeg";
+import myProjectsImg from "../assets/Crafting_Table-projects.png";
+import galleryImg from "../assets/Baroque-gallery.png";
+import storeImg from "../assets/Oak_Hanging_Sign-store.png";
+import aboutImg from "../assets/Bee-about.webp";
+import contactImg from "../assets/Writable_Book-contact.png";
+import cartImg from "../assets/Chest-cart.png";
+import QuickLinks from "./QuickLinks.jsx";
 
 function WindowHome() {
     return (
@@ -10,33 +16,13 @@ function WindowHome() {
                     <div className={styles.closeButton}>x</div>
                 </div>
                 <div className={styles.quickLinksContainer}>
-
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">My Projects</a>
-                    </div>
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">Gallery</a>
-                    </div>
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">Store</a>
-                    </div>
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">About Me</a>
-                    </div>
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">Contact Me</a>
-                    </div>
-                    <div className={styles.quickLinksItem}>
-                        <a href="#"><img src={myProjectsImg} className={styles.windowImg}/></a>
-                        <a href="#">Your Cart</a>
-                    </div>
+                    <QuickLinks image={myProjectsImg} name= "My Projects"/>
+                    <QuickLinks image={galleryImg} name= "Gallery"/>
+                    <QuickLinks image={storeImg} name= "Store"/>
+                    <QuickLinks image={aboutImg} name= "About"/>
+                    <QuickLinks image={contactImg} name= "Contact"/>
+                    <QuickLinks image={cartImg} name= "Cart"/>
                 </div>
-
             </div>
         </div>
     );
