ae88c99a73a00feb5b476f6161327ca9ff5aff83
sarakok <kok.sara777@gmail.com>
2025-07-21 19:05:56 +1000

    Created new page for projects

---
+++ my-website/src/Pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "../Components/Header.jsx";
+
+function Projects () {
+    return (
+        <div>
+            <Header/>
+            MyProjects</div>
+    )
+}
+
+export default Projects
