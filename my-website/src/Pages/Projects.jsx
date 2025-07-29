09aa3d2d0b6ae95ae5f32398de0de437cd6d445a
sarakok <kok.sara777@gmail.com>
2025-07-29 21:57:25 +1000

    Added images for projects

--- my-website/src/Pages/Projects.jsx
+++ my-website/src/Pages/Projects.jsx
@@ -3,6 +3,8 @@ import Header from "../Components/Header.jsx";
 import Window from "../Components/Window.jsx";
 import styles from "../../styles/Projects.module.css";
 import redLine from "../assets/projects-page-line.svg"
+import myWebsiteImg from "../assets/projects-mywebsite.png";
+import ecomImg from "../assets/projects-ecommerce.png";
 
 function Projects () {
     return (
@@ -17,7 +19,7 @@ function Projects () {
                 </div>
             </div>
             <div className={styles.projectItem}>
-                <Window title="My Website"/>
+                <Window title="My Website"  image={myWebsiteImg} />
                 <div className={styles.projectDescription}>
                     <p>
                         You’re looking at it! This is my personal website which showcases all the projects I have done so far.
@@ -36,17 +38,18 @@ function Projects () {
                 <div className={styles.projectDescription}>
                     <p>
                         This is a group project built by three developers as part of a university web systems development unit.
-                        The application was created for a fictional business client and enables staff to manage products, coupons, orders, and customer data through a secure and modular CakePHP-based web system.
-                        A front facing ... has also been created for customer to purchase goods.
+                        The application was created for a fictional business client and enables staff to manage products, coupons, orders,
+                        and customer data through a secure and modular CakePHP-based web system.
                         It was designed with maintainability and future scalability in mind.
                     </p>
                     <div >
-                        <p>Go to <a href="https://review.u25s1052.iedev.org/">website</a></p>
-                        <p>Go to <a href="https://github.com/sarakcw/E-commerce-Platform">github</a></p>
+                        <p>Go to <a href="https://review.u25s1052.iedev.org/">website</a></p><br/>
+                        <p>Log into the admin dashboard! <br/> Username: <span style={{color: "#A44A3F"}}>test@admin.com</span> <br/> Password: <span style={{color: "#A44A3F"}}>asdfasdf</span></p>
+
                     </div>
 
                 </div>
-                <Window title="E-commerce Platform" />
+                <Window title="E-commerce Platform" image={ecomImg} />
             </div>
             <div className={styles.projectItem}>
                 <Window title="Java Playable Game" />
